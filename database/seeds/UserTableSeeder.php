8be42f19de8bdeeac36ae4ad6ec37e61d139b09a
Edward Taroghion <edwardt2001@hotmail.com>
2015-04-13 16:34:02 +0100

    Create User Table Seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\User;
+
+class UserTableSeeder extends Seeder {
+ 
+    public function run()
+    {
+ 
+        Eloquent::unguard();
+ 
+        DB::table('users')->delete();
+ 
+        $faker = Faker\Factory::create();
+
+        User::create(array(
+            'name' => 'foobar',
+            'email' => 'hi@me.com',
+            'password' => Hash::make('password'),
+        ));
+ 
+ 
+        for($i = 0; $i < 99; $i++){
+            User::create(array(
+                'name' => $faker->userName,
+                'email' => $faker->email,
+                'password' => Hash::make($faker->name . $faker->year),
+            ));
+        }
+ 
+    }
+ 
+}
